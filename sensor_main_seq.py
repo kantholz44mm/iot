5a14abc1f6c39ed6ddd2671de520891d993a6365
nico-hub-coding-101 <xnicolas.the@gmail.com>
2025-12-08 15:48:42 +0100

    Create sensor_main_seq.py; sends one json every t seconds

---
+++ sensor_main_seq.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+from gpiozero import DistanceSensor, MotionSensor, Button
+from signal import pause
+import time
+from datetime import datetime
+import json
+from time import sleep
+
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    """Wird aufgerufen, wenn der Client eine Verbindung zum Broker herstellt."""
+    if rc == 0:
+        print("Verbunden mit MQTT Broker.")
+    else:
+        print(f"Verbindung fehlgeschlagen, Return Code {rc}")
+
+def publish(client,sensor, value_sensor, timestamp_sensor, topic):
+   
+    msg = json.dumps({"Sensor" : sensor, "Value" : value_sensor, "Time" : timestamp_sensor })
+    #msg = f"{sensor} : {message}"
+    result = client.publish(topic, msg)
+    status = result[0]
+    
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+def publish_all(client, topic):
+    dst_ultra = str(u_sonic_sensor.distance)
+    timestamp_sensor = str(datetime.now())
+    msg = json.dumps({"Sensor_PIR" : pir_detect, "Sensor_Ultra" : dst_ultra, "Sensor_Sound" : sound_detect, "Time" : timestamp_sensor},)
+    #msg = f"{sensor} : {message}"
+    result = client.publish(topic, msg)
+    status = result[0]
+    
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
+def set_pir():
+    global pir_detect
+    print("Pir detected")
+    pir_detect = 1
+
+def set_sound():
+    global sound_detect
+    sound_detect = 1
+
+def set_ultra():
+    ultra_dist = str(u_sonic_sensor.distance)
+
+def reset_vals():
+    global pir_detect
+    global sound_detect
+    pir_detect = 0
+    sound_detect = 0
+    
+
+audio_sensor = Button(22, pull_up=None, active_state=False)
+motion_sensor = Button(27, pull_up=None, active_state=False)
+u_sonic_sensor = DistanceSensor(18, 17, max_distance=2, threshold_distance=0.5)
+
+broker = 'broker.hivemq.com'
+port = 1883
+topic = "iot/sensors"
+client_id = f'python-mqtt-55323'
+
+pir_detect = 0
+sound_detect = 0
+client = mqtt.Client(client_id=client_id)
+client.on_connect = on_connect
+client.connect(broker, port)
+
+audio_sensor.when_pressed = set_sound
+motion_sensor.when_pressed = set_pir
+
+client.loop_start()
+
+try:
+    while True:
+        publish_all(client, topic)
+        reset_vals()
+        sleep(0.3)
+finally:
+
+    client.loop_stop()
+    client.disconnect()
+    print("Verbindung getrennt.")
+
+
+#TODO: PIR Sensor als genauere Aktivierung für Ultrasonic, um dann dort distance auszulesen.
+#Objekte, welche immer im schwellwert sitzen blockieren die in range methode. vielleicht flag setzen, wenn in range und out of range
